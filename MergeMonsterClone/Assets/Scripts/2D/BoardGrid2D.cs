81faccb4d9efff51b029c978be24c02e02d8d58d
Furkan Elmas <furkanelmas321@gmail.com>
2022-08-08 22:02:22 +0300

    Created grid system scripts

---
+++ MergeMonsterClone/Assets/Scripts/2D/BoardGrid2D.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using System;
+
+public class BoardGrid2D<TGridObject>
+{
+    int _width;
+    int _height;
+    float _cellSize;
+    Vector3 _originPosition;
+
+    TGridObject[,] _gridTiles;
+
+    public int Width => _width;
+    public int Height => _height;
+    public float CellSize => _cellSize;
+    public Vector3 OriginPosition => _originPosition;
+
+
+    public BoardGrid2D(int width, int height, float cellSize, Vector3 originPosition, Func<BoardGrid2D<TGridObject>, int, int, TGridObject> createGridObject)
+    {
+        if (width < 0 || height < 0)
+            return;
+
+        _width = width;
+        _height = height;
+        _cellSize = cellSize;
+        _originPosition = originPosition;
+
+        _gridTiles = new TGridObject[width, height];
+
+        for (int i = 0; i < _gridTiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < _gridTiles.GetLength(1); j++)
+            {
+                _gridTiles[i, j] = createGridObject(this, i, j);
+
+                Debug.DrawLine(GetWorldPosition(i, j) - Vector3.one * _cellSize / 2, GetWorldPosition(i, j + 1) - Vector3.one * _cellSize / 2, Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(i, j) - Vector3.one * _cellSize / 2, GetWorldPosition(i + 1, j) - Vector3.one * _cellSize / 2, Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(width, 0) - Vector3.one * _cellSize / 2, GetWorldPosition(width, height) - Vector3.one * _cellSize / 2, Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(0, height) - Vector3.one * _cellSize / 2, GetWorldPosition(width, height) - Vector3.one * _cellSize / 2, Color.white, 100f);
+    }
+
+    public Vector3 GetWorldPosition(int row, int column)
+    {
+        return new Vector3(row, column) * _cellSize + _originPosition + Vector3.one * _cellSize / 2;
+    }
+
+    public void GetRowAndColumn(Vector3 worldPosition, out int row, out int column)
+    {
+        row = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+        column = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
+    }
+
+    public void SetGridObject(int row, int column, TGridObject obj)
+    {
+        if (row >= 0 && column >= 0 && row < _width && column < _height)
+        {
+            _gridTiles[row, column] = obj;
+        }
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TGridObject obj)
+    {
+        int row, column;
+        GetRowAndColumn(worldPosition, out row, out column);
+        SetGridObject(row, column, obj);
+    }
+
+    public TGridObject GetGridObject(int row, int column)
+    {
+        if (row >= 0 && column >= 0 && row < _width && column < _height)
+        {
+            return _gridTiles[row, column];
+        }
+        else
+            return default(TGridObject);
+    }
+
+    public TGridObject GetGridObject(Vector3 worldPosition)
+    {
+        int row, column;
+        GetRowAndColumn(worldPosition, out row, out column);
+        return GetGridObject(row, column);
+    }
+}
---
+++ MergeMonsterClone/Assets/Scripts/3D/Testing3D.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+
+public class Testing3D : MonoBehaviour
+{
+    [SerializeField] int _cellWidth = 8;
+    [SerializeField] int _cellHeight = 8;
+    [SerializeField] float _cellSize = 1.0f;
+
+    [SerializeField] Vector3 _originPosition = Vector3.zero;
+
+    [SerializeField] GameObject _boardTilePrefab;
+    [SerializeField] GameObject _characterPrefab;
+
+    [SerializeField] LayerMask _characterLayerMask;
+    [SerializeField] LayerMask _groundLayerMask;
+
+    float _characterOffset;
+
+    GameObject _pickedGameObject;
+
+    BoardGrid3D<Tile> _boardGrid;
+    Tile _lastPickedTile;
+
+
+    void Awake()
+    {
+        _boardGrid = new BoardGrid3D<Tile>(_cellWidth, _cellHeight, _cellSize, _originPosition, CreateNode);
+        GameObject tileContainer = GameObject.Find("TileContainer");
+        if (tileContainer == null)
+            tileContainer = new GameObject("TileContainer");
+
+        for (int i = 0; i < _boardGrid.Width; i++)
+        {
+            for (int j = 0; j < _boardGrid.Height; j++)
+            {
+                GameObject spawnedTile = InstantiateGameObject(_boardTilePrefab, _boardGrid.GetWorldPosition(i, j), Quaternion.Euler(90, 0, 0));
+                spawnedTile.name = $"Tile3D [{i}, {j}]";
+                spawnedTile.transform.SetParent(tileContainer.transform);
+            }
+        }
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Tile tile;
+
+        bool characterHit = Physics.Raycast(ray, out RaycastHit hitCharacterObj, 1000f, _characterLayerMask);
+        bool groundHit = Physics.Raycast(ray, out RaycastHit hitGroundObj, 1000f, _groundLayerMask);
+
+        if (characterHit)
+        {
+            tile = _boardGrid.GetGridObject(hitCharacterObj.point);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                _lastPickedTile = tile;
+
+                tile = _boardGrid.GetGridObject(hitCharacterObj.point);
+                _pickedGameObject = hitCharacterObj.transform.gameObject;
+                _characterOffset = _pickedGameObject.GetComponent<MeshRenderer>().bounds.size.y / 2;
+                SetTileState(tile, true);
+                return;
+            }
+        }
+
+        if (groundHit)
+        {
+            tile = _boardGrid.GetGridObject(hitGroundObj.point);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (tile == null) return;
+
+                _lastPickedTile = tile;
+
+                if (tile.IsAvailable)
+                {
+                    _boardGrid.GetRowAndColumn(hitGroundObj.point, out int row, out int column);
+                    _characterOffset = _characterPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
+                    InstantiateGameObject(_characterPrefab, _boardGrid.GetWorldPosition(row, column) + Vector3.up * _characterOffset, Quaternion.identity);
+                    SetTileState(tile, false);
+                    _pickedGameObject = null;
+                }
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                if (_pickedGameObject != null)
+                {
+                    SetObjectPosition(_pickedGameObject, hitGroundObj.point, _characterOffset);
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (_pickedGameObject == null)
+                    return;
+
+                tile = _boardGrid.GetGridObject(hitGroundObj.point);
+
+                if (tile != null)
+                {
+                    if (!tile.IsAvailable)
+                    {
+                        SetObjectPosition(_pickedGameObject, GetTilePosition(_lastPickedTile), _characterOffset);
+                        SetTileState(_lastPickedTile, false);
+                    }
+                    else
+                    {
+                        SetObjectPosition(_pickedGameObject, GetTilePosition(tile), _characterOffset);
+                        SetTileState(tile, false);
+                        if (_lastPickedTile != tile)
+                            SetTileState(_lastPickedTile, true);
+                    }
+                }
+                else
+                {
+                    SetObjectPosition(_pickedGameObject, GetTilePosition(_lastPickedTile), _characterOffset);
+                    SetTileState(_lastPickedTile, false);
+                }
+
+                _pickedGameObject = null;
+            }
+        }
+    }
+
+    void SetObjectPosition(GameObject obj, Vector3 pos, float offset)
+    {
+        obj.transform.position = pos + Vector3.up * offset;
+    }
+
+    void SetTileState(Tile tile, bool state)
+    {
+        tile.IsAvailable = state;
+    }
+
+    Vector3 GetTilePosition(Tile tile)
+    {
+        return tile.Get3DTilePosition();
+    }
+
+    Tile CreateNode(BoardGrid3D<Tile> grid, int row, int column)
+    {
+        return new Tile(grid, row, column);
+    }
+
+    GameObject InstantiateGameObject(GameObject obj, Vector3 pos, Quaternion angle)
+    {
+        return Instantiate(obj, pos, angle);
+    }
+}
