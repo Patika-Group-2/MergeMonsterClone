bfe56521659416409f0e3580275e29db5347c18a
Furkan Elmas <furkanelmas321@gmail.com>
2022-08-16 23:49:01 +0300

    Added animations

--- Assets/MergeMonsterClone/Assets/Scripts/ScriptsO/ReFactor/MeleeAnimator.cs
+++ Assets/MergeMonsterClone/Assets/Scripts/ScriptsO/ReFactor/MeleeAnimator.cs
@@ -8,10 +8,22 @@ public class MeleeAnimator : MonoBehaviour
     {
         _meleeAnimator = GetComponent<Animator>();
         GetComponent<MeleeAttack>().OnAttack += AttackAnimation;
+        GetComponent<Movement>().OnMove += Move;
+        GetComponent<Movement>().OnTargetClose += Stop;
     }
 
     public void AttackAnimation()
     {
-        // _meleeAnimator.SetTrigger("Attack");
+        _meleeAnimator.SetTrigger("Attack");
+    }
+
+    void Move()
+    {
+        _meleeAnimator.SetBool("IsMoving", true);
+    }
+
+    void Stop(Transform target)
+    {
+        _meleeAnimator.SetBool("IsMoving", false);
     }
 }
