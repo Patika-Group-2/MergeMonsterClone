029da81aba7de63f348194b5186cf045d9316b6b
Furkan Elmas <furkanelmas321@gmail.com>
2022-08-18 22:04:19 +0300

    Edited main menu scene

--- Assets/MergeMonsterClone/Assets/Scripts/ScriptsO/ReFactor/MainMenuButtonHandler.cs
+++ Assets/MergeMonsterClone/Assets/Scripts/ScriptsO/ReFactor/MainMenuButtonHandler.cs
@@ -1,14 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MainMenuButtonHandler : MonoBehaviour
 {
     [SerializeField] AudioClip _buttonClickSound;
     private AudioSource _source;
 
+    [SerializeField] PostProcessVolume _volume;
+
+    AutoExposure _autoExposure;
+
     private void Awake()
     {
         _source = GetComponent<AudioSource>();
+        _volume.profile.TryGetSettings(out _autoExposure);
     }
     public void QuitButton()
     {
@@ -19,6 +26,13 @@ public class MainMenuButtonHandler : MonoBehaviour
     public void PlayButton()
     {
         _source.PlayOneShot(_buttonClickSound);
+        StartCoroutine(ShowCor());
+    }
+
+    IEnumerator ShowCor()
+    {
+        _autoExposure.keyValue.value = 0;
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
