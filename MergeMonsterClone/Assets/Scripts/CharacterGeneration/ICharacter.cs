2f8e59e86b161bcd7f9fde9365ca8e81439ecec8
Okan Canpolat <okancanpolat002344@hotmail.com>
2022-08-22 12:14:55 +0300

    unnecessary files deleted

--- MergeMonsterClone/Assets/Scripts/CharacterGeneration/ICharacter.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface ICharacter
-{
-    public GameObject CharacterPrefab { get; set; }
-
-    public void PositionCharacter(Vector3 position, Quaternion rotation);
-    public GameObject GenerateCharacter();
-}
